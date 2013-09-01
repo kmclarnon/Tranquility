36182648a65ae6468b440fd7e790c9de3f73d973
Kevin McLarnon <mclarnon.k@gmail.com>
2013-09-01 19:44:09 -0400

    Test scene
    Added some basic tests of rotation and scaling

--- Engine/Scene.cpp
+++ Engine/Scene.cpp
@@ -26,7 +26,27 @@ bool Scene::init(int width, int height)
 
 bool Scene::update()
 {
+    static float rotation = 0.0f;
+    static float scale = 1.0f;
+
+    // update the camera
     this->camera.update();
+
+    // Update the rotation variable each frame.
+    rotation += 0.0174532925f * 1.0f;
+    if(rotation > 360.0f)
+    {
+        rotation -= 360.0f;
+    }
+
+    scale -= 0.001;
+    if(scale < 0.5f)
+    {
+        scale = 1.0f;
+    }
+
+    this->model.setOrientation(glm::quat(glm::vec3(rotation, rotation, 0.0f)));
+    this->model.setScale(scale);
 
     return true;
 }
