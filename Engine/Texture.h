2afea871a861e545840d18a27798da1f0037c390
Kevin McLarnon <mclarnon.k@gmail.com>
2013-09-01 22:56:51 -0400

    Ignore ImageMagick++ compiler warnings
    Only wish to see warnings from the code we actually own

--- Engine/Texture.h
+++ Engine/Texture.h
@@ -2,7 +2,12 @@
 #define _TRILLEK_TEXTURE_H_
 
 #include "Common.h"
+
+#pragma warning(push)
+#pragma warning(disable: 4251)
+#pragma warning(disable: 4244)
 #include "Magick++.h"
+#pragma warning(pop)
 
 class Texture
 {
