1b123d15bcb28ec53a3182548ef19e58fb6895c1
Kevin McLarnon <mclarnon.k@gmail.com>
2013-09-06 17:51:10 -0400

    Added FPSCamera functionality
    FPSCamera can now be moved up and moved down in a similar fashion to
    moveLeft, moveRight etc.

--- Engine/FPSCamera.cpp
+++ Engine/FPSCamera.cpp
@@ -2,8 +2,8 @@
 
 FPSCamera::FPSCamera()
     : frameTime(0.0), forwardSpeed(0.0), backwardSpeed(0.0), leftSpeed(0.0), 
-    rightSpeed(0.0), forwardStepSpeed(0.003), backwardStepSpeed(0.001), 
-    maxSpeed(0.01)
+    rightSpeed(0.0), acelStepSpeed(0.003), decelStepSpeed(0.001), 
+    maxSpeed(0.01), upSpeed(0.0), downSpeed(0.0)
 {
 
 }
@@ -11,7 +11,9 @@ FPSCamera::FPSCamera()
 void FPSCamera::update()
 {
     // update our position based on our speed
-    this->move((float)this->rightSpeed - (float)this->leftSpeed, 0.0f, (float)this->forwardSpeed - (float)this->backwardSpeed);
+    this->move((float)this->rightSpeed - (float)this->leftSpeed,
+                (float)this->upSpeed - (float)this->downSpeed,
+                (float)this->forwardSpeed - (float)this->backwardSpeed);
 
     this->Camera::update();
 }
@@ -36,17 +38,27 @@ void FPSCamera::moveRight(double time, bool keyDown)
     this->moveEx(time, keyDown, this->rightSpeed);
 }
 
+void FPSCamera::moveUp(double time, bool keyDown)
+{
+    this->moveEx(time, keyDown, this->upSpeed);
+}
+
+void FPSCamera::moveDown(double time, bool keyDown)
+{
+    this->moveEx(time, keyDown, this->downSpeed);
+}
+
 void FPSCamera::moveEx(double time, bool keyDown, double &speed)
 {
     if(keyDown)
     {
-        speed += time * this->forwardStepSpeed;
+        speed += time * this->acelStepSpeed;
         if(speed > (time * this->maxSpeed))
             speed = time * this->maxSpeed;
     }
     else
     {
-        speed -= time * this->backwardStepSpeed;
+        speed -= time * this->decelStepSpeed;
         if(speed < 0.0)
             speed = 0.0;
     }
--- Engine/FPSCamera.h
+++ Engine/FPSCamera.h
@@ -12,17 +12,20 @@ public:
     virtual void update();
     void moveForward(double time, bool keyDown);
     void moveBackward(double time, bool keyDown);
-    void moveLeft(double time,bool keyDown);
-    void moveRight(double time,bool keyDown);
+    void moveLeft(double time, bool keyDown);
+    void moveRight(double time, bool keyDown);
+    void moveUp(double time, bool keyDown);
+    void moveDown(double time, bool keyDown);
 
 private:
     void moveEx(double time, bool keyDown, double &speed);
 
     double frameTime;
-    double forwardStepSpeed, backwardStepSpeed, maxSpeed;
+    double acelStepSpeed, decelStepSpeed, maxSpeed;
 
     double forwardSpeed, backwardSpeed;
     double leftSpeed, rightSpeed;
+    double upSpeed, downSpeed;
 };
 
 #endif
